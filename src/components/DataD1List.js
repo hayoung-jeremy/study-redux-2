a65f55d0f7a5884b0a6404b4bf6f4ca282b1fb02
Ha young, Kim <jeremiah91@naver.com>
2021-05-28 22:19:53 +0900

    cannot filter data

--- src/components/DataD1List.js
+++ src/components/DataD1List.js
@@ -5,21 +5,21 @@ import { v4 as uuidv4 } from "uuid";
 const DataD1List = () => {
   // 광주은행 메뉴트리
   const [KJ_SB] = data_personal;
-  console.log(KJ_SB.menus);
-  // depth 01
-  const depth_01 = KJ_SB.menus.map((item) => {
-    const depth_01__title = item.app_name;
 
-    return (
-      <li key={uuidv4()} className="btn">
-        {depth_01__title}
-      </li>
-    );
-  });
   return (
     <>
       <div className="default-container middle">
-        <ul className="flex-container">{depth_01}</ul>
+        <ul className="flex-container">
+          {KJ_SB.menus.map((item) => {
+            const depth_01__title = item.app_name;
+            // depth_01
+            return (
+              <li key={uuidv4()} className="btn">
+                {depth_01__title}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </>
   );
--- src/components/DataD2List.js
+++ src/components/DataD2List.js
@@ -5,24 +5,27 @@ import { v4 as uuidv4 } from "uuid";
 const DataD2List = () => {
   // 광주은행 메뉴트리
   const [KJ_SB] = data_personal;
-  // depth 02
-  const depth_02 = KJ_SB.menus.map((item) =>
-    item.menus.map((item) => {
-      const depth_02__title = item.app_name;
-      const depth_02__path = item.app_url;
-      return (
-        <li key={uuidv4()} className="btn">
-          {depth_02__title} {depth_02__path}
-        </li>
-      );
-    })
-  );
+  console.log(KJ_SB.menus);
 
-  return (
-    <div>
-      <ul>{depth_02}</ul>
-    </div>
+  const depth_01__arr = KJ_SB.menus;
+  const depth_01__id = depth_01__arr.map((item) => item.id);
+  const depth_02__pid = depth_01__arr.map((item) =>
+    item.menus.map((item) => item.pid)
   );
+  console.log(depth_02__pid);
+  console.log(depth_01__id);
+  const depth_02__arr = depth_01__arr.map(function (item, index) {
+    const depth_02__menus = item.menus.map((item) => item.menus);
+
+    const filtered_02_menus = depth_02__menus.filter(function (item) {
+      const depth_02__pid = item.map((item) => item.pid);
+      return depth_02__pid === depth_01__id;
+    });
+    console.log(filtered_02_menus);
+    return <li>{filtered_02_menus}</li>;
+  });
+
+  return <div className="default-container middle">{depth_02__arr}</div>;
 };
 
 export default DataD2List;
