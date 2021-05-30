66bf9d88332f025ad47539ab02384629766ef1f5
Ha young, Kim <jeremiah91@naver.com>
2021-05-31 07:43:48 +0900

    children of 메인

--- src/components/DataD2List.js
+++ src/components/DataD2List.js
@@ -3,16 +3,30 @@ import data_personal from "../db/data_personal.json";
 import { v4 as uuidv4 } from "uuid";
 
 const DataD2List = () => {
+  let d2_index = 0;
+
   // 광주은행 메뉴트리
   const [KJ_SB] = data_personal; // [ {} ]
 
   const depth_01__arr = KJ_SB.menus;
-  console.log(depth_01__arr); // [ {}, {}, {} ... ]
+  // console.log(depth_01__arr); // [ {}, {}, {} ... ]
 
-  console.log(depth_01__arr[0].menus);
+  const depth_02__arr = depth_01__arr.map((item) => {
+    return item.menus;
+  });
+
+  const depth_02__title = depth_02__arr.map((item) => {
+    const dp_2_title = item.map((item) => {
+      return <li key={uuidv4()}>{item.app_name}</li>;
+    });
+    return dp_2_title;
+  });
+  console.log(depth_02__title);
 
   return (
-    <div className="default-container middle">{depth_01__arr[0].menus}</div>
+    <div className="default-container middle">
+      <ul>{depth_02__title[d2_index]}</ul>
+    </div>
   );
 };
 
