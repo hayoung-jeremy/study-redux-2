7e6cf0dc3c6f18a8dc8fcdb91dcc02996771aa0f
Ha young, Kim <jeremiah91@naver.com>
2021-05-31 16:48:42 +0900

    onClick button and hover effect

--- src/components/DataD1List.js
+++ src/components/DataD1List.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import data_personal from "../db/data_personal.json";
 import { v4 as uuidv4 } from "uuid";
 import { Link } from "react-router-dom";
@@ -7,15 +7,20 @@ const DataD1List = () => {
   // 광주은행 메뉴트리
   const [KJ_SB] = data_personal;
 
-  const ListItems = ({ value }) => (
-    <li className="btn">
+  const [clickedItem, setClickedItem] = useState(0);
+
+  const ListItems = ({ index, value }) => (
+    <li
+      className={`btn ${clickedItem === index ? "btn-clicked" : ""}`}
+      onClick={() => setClickedItem(index)}
+    >
       <Link>{value}</Link>
     </li>
   );
 
   const TitleList = () => {
     const depth_01_title = KJ_SB.menus.map((item, index) => {
-      return <ListItems key={uuidv4()} value={item.app_name} />;
+      return <ListItems key={uuidv4()} value={item.app_name} index={index} />;
     });
     return <ul className="flex-container">{depth_01_title}</ul>;
   };
